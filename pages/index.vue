5e0936f346985bb78195f2f3e2980196179b7baa
Chiaki Uehira <me@uhck.in>
2019-10-21 15:54:52 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <img src="/logo.svg" alt="">
+    <p>準備中です</p>
   </div>
 </template>
 
@@ -18,10 +19,15 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   text-align: center;
   padding: 0 24px;
+  color: #fff;
+  font-weight: bold;
+  text-align: center;
 }
 img {
   max-width: 100%;
+  margin-bottom: 24px;
 }
 </style>
